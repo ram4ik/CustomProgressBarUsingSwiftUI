00581194eb4be541d8c4f2d051af39f7c50563fc
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-02-07 22:00:17 +0200

    init

--- CustomProgressBarUsingSwiftUI/ContentView.swift
+++ CustomProgressBarUsingSwiftUI/ContentView.swift
@@ -9,13 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var percent: CGFloat = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            CustmoProgressView(percent: self.$percent)
+        }.padding()
+        .onTapGesture {
+            self.percent = 0.75
+        }.animation(.spring())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustmoProgressView: View {
+    @Binding var percent: CGFloat
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            ZStack(alignment: .trailing) {
+                Capsule()
+                    .fill(Color.black.opacity(0.08))
+                    .frame(height: 22)
+                Text(String(format: "%.0f", self.percent * 100) + "%")
+                    .font(.caption)
+                    .foregroundColor(Color.gray.opacity(0.75))
+                    .padding(.trailing)
+            }
+            Capsule()
+                .fill(LinearGradient(gradient: .init(colors: [Color("Color1"), Color("Color2")]), startPoint: .leading, endPoint: .trailing))
+                .frame(width: self.calPercent(), height: 22)
+            
+        }
+        .padding(18)
+        .background(Color.black.opacity(0.085))
+        .cornerRadius(15)
+    }
+    
+    func calPercent() -> CGFloat {
+        let width = UIScreen.main.bounds.width - 66
+        return width * self.percent
     }
 }
